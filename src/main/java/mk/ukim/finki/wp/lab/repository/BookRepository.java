05d1d3eb6ef9f61ac93f56ca793818c8d06446df
jewinator-8 <djonceskidj@gmail.com>
2025-10-30 23:29:28 +0100

    fix

--- src/main/java/mk/ukim/finki/wp/lab/repository/BookRepository.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/BookRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface BookRepository {
     List<Book> findAll();
     List<Book> searchBooks(String text, Double rating);
--- src/main/java/mk/ukim/finki/wp/lab/repository/BookReservationRepository.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/BookReservationRepository.java
@@ -3,7 +3,6 @@ package mk.ukim.finki.wp.lab.repository;
 import mk.ukim.finki.wp.lab.model.BookReservation;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookReservationRepository {
     BookReservation save(BookReservation reservation);
 }
